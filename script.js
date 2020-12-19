009e5aaca4ba0b7efa6c59daad92be005cf5de8b
Code Malayalam <msmailbox@gmail.com>
2020-12-19 15:00:46 +0000

    home pate done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-        fillListItems('clinics.json', createClinicItem)
+        fillListItems('clinics.json', 'clinics', createClinicItem);
+        fillListItems('faq.json', 'faq', createFaqItem);
 }
 
 function gotoFaq(id) {
@@ -10,15 +11,48 @@ function gotoClinic(id) {
     console.log('CLINIC ID=', id);
 }
 
-function fillListItems(url, cb) {
+function fillListItems(url,id, cb) {
     fetch(url + '?v=2')
     .then(response => response.json())
     .then((data = []) => {
         const dom = data.map(cb).join(' ');
-        var node = document.getElementById("clinics");
+        var node = document.getElementById(id);
         node.insertAdjacentHTML('beforeend', dom);
     });
 }
+
+function createFaqItem(item) {
+    const {
+        id,
+        mal = {},
+        img,
+    }  = item;
+
+    const {
+        q,
+        a
+    } = mal;
+
+    return `
+        <div class="faq-item section-item" onclick="gotoFaq(${id})">
+            ${img ? `
+                <div class="faq-img">
+                    <img src="images/${img}" alt="logo" width="100px" height="100px">
+                </div>
+            ` : ''}
+            <div>
+                <div class="faq-item-header">
+                    ${q}
+                </div>
+                <div class="faq-item-body">
+                    ${a}
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+
 
 function createClinicItem(item) {
     const {
@@ -33,7 +67,7 @@ function createClinicItem(item) {
     return `
         <div class="clinic-item section-item" onclick="gotoClinic(${id})">
             <div class="clinic-img">
-                <img src="${icon}" alt="logo" width="40px" height="40px">
+                <img src="icons/${icon}" alt="logo" width="40px" height="40px">
             </div>
             <div class="clinic-body">
                 <div class="clinic-title">
