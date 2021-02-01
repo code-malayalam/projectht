4cafc09af647fb632ad0315e1e82c6e148a62d09
Code Malayalam <msmailbox@gmail.com>
2021-02-01 11:57:14 +0000

    Modified script

--- scriptClinics.js
+++ scriptClinics.js
@@ -1,10 +1,23 @@
 window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const place = urlParams.get('place');
+    console.log(place);
+
     const reqs = [
         {
             url: '/api/clinics.json',
             id: 'clinics',
-            cb: createClinicItem
+            cb: createClinicItem,
+            
         }
     ];
-    loadData(reqs);
+    loadData(reqs, (data) => {
+        if(!place) {
+            return data;
+        }
+        return data.filter(({places = []}) => {
+            console.log(places);
+            return places.find((pItem) => pItem.toLocaleLowerCase() === place.toLocaleLowerCase());
+        });
+    });
 }
